d5f079ec415fb0529d0046feca64f46e8c79e23b
opollopo <271310065@qq.com>
2021-10-17 14:49:12 +0800

    flask动态路由

--- 401/app.py
+++ 401/app.py
@@ -6,9 +6,16 @@ app = flask.Flask(__name__)
 @app.route("/")
 def home():
     return 'hello'
+
+
 @app.route("/h")
 def hom():
     return 'wrold'
+
+
+@app.route("/a/<int:name>")
+def dy(name):
+    return 'wrold%s' % name
 
 
 if __name__ == '__main__':
