3ec67acb3911d27aabcea9d236224cceb6178eb0
opollopo <271310065@qq.com>
2021-10-22 20:06:05 +0800

    添加静态文件

--- 401/app.py
+++ 401/app.py
@@ -1,6 +1,6 @@
 import flask
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__, static_url_path='')
 
 
 @app.route("/")
@@ -19,4 +19,5 @@ def dy(name):
 
 
 if __name__ == '__main__':
+    print(app.url_map)
     app.run()
